cecdec82dbe3671f677a22d2212853421d518805
沈煜 <chrisniael@gmail.com>
2014-07-16 23:20:12 +0800

    add frontend module

--- frontend/views/site/agency.php
+++ frontend/views/site/agency.php
@@ -1,3 +1,8 @@
+<?php
+
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+?>
 <!--wrapper start -->
 <div class="wrapper">
     <div class="login-panel">
@@ -7,18 +12,22 @@
                 <span id="err_tips" class="vm"></span>
             </div>
 
-            <form id="login-form" class="login-form">
-                <div class="login-un">
-                    <span class="icon-wrapper">
-                        <i class="icon_login un"></i>
-                    </span>
-                    <input id="account" type="text" placeholder="代理号"></input>
-                </div>    
-            </form>
+            <form id="login-form" class="login-form" action="index.php?r=site/agecy">
+            <?php // $form = ActiveForm::begin(['options'=>['id' => 'login-form', 'class' => 'login-form']]) ?>
+
+            <div class="login-un">
+                <span class="icon-wrapper">
+                    <i class="icon_login un"></i>
+                </span>
+                <input id="account" type="text" placeholder="代理号"></input>
+                <?php //  Html::textInput('code', '', ['id'=>'account', 'placeholder'=>"代理号"]) ?>
+            </div>    
+            <!--</form>-->
 
             <div class="login-btn-panel">
                 <a id="login_button" class="login-btn" href="javascript:;" title="点击查询">查询</a>
             </div>
+            <?php // ActiveForm::end(); ?>
         </div>
     </div>
 </div>
