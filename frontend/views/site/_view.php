956144f5e9fe1ab256bb22e78082427cdf97121b
沈煜 <chrisniael@gmail.com>
2014-08-07 17:50:47 +0800

    Modify article list style.

---
+++ frontend/views/site/_view.php
@@ -0,0 +1,2 @@
+<div class="listItemUrl"><?= $model->url ?></div>
+<div class="listItemCT"><?php $date = $model->create_time; echo substr($date, 0, 10); ?></div>
--- frontend/views/site/article.php
+++ frontend/views/site/article.php
@@ -4,39 +4,31 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use common\models\search\ArticleSearch;
 use common\models\Article;
-use yii\data\ArrayDataProvider;
+use yii\data\ActiveDataProvider;
+use yii\widgets\ListView;
 ?>
 
 <?php if(empty($id)) : ?>
 <div class="article-index">
 
     <?php
-    $query = Article::find()->asArray()->all();
+    $query = Article::find();
 
-    $dataProvider = new ArrayDataProvider([
-        'allModels' => $query,
+    $dataProvider = new ActiveDataProvider([
+        'query' => $query,
     ]);
     ?>
 
     <?=
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns'      => [
-            [
-                'class' => 'yii\grid\SerialColumn'
-            ],
-            [
-                'attribute' => 'url',
-                'label'     => '标题',
-                'format' => 'html',
-            ],
-            [
-                'attribute' => 'create_time',
-                'label' => '创建时间',
-                'format' => 'date',
-            ]
-        // 'update_time',
+    ListView::widget([
+        'itemOptions' => [
+            'class' => "listItem",
         ],
+        'dataProvider' => $dataProvider,
+        'itemView' => '_view',
+         'pager'=>array(  
+                'maxButtonCount'=>'5',
+                ),
     ]);
     ?>
 
